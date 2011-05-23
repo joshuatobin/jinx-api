78db90b1aa0130f6457a4971fe2580f2d7c82d85
Joshua Tobin <joshuatobin@gmail.com>
2011-05-23 15:56:31 -0700

    Adding pdu unit tests for real this time

---
+++ src/jinx_api/api/tests/views/pdu.py
@@ -0,0 +1,20 @@
+from api.tests.base import JinxTestCase
+import clusto
+from llclusto.drivers import LindenPDU
+import sys
+
+class TestGetPduHostnames(JinxTestCase):
+    api_call_path = "/jinx/2.0/get_pdu_hostnames"
+
+    def data(self):
+        # Populate Clusto
+        pdu1 = LindenPDU()
+        pdu2 = LindenPDU()
+        pdu1.hostname = "pdu1.lindenlab.com"
+        pdu2.hostname = "pdu2.lindenlab.com"
+    
+    def test_normal_call(self):
+        response = self.do_api_call()
+        self.assert_response_code(response, 200)
+        self.assertEqual(sorted(response.data), ["pdu1.lindenlab.com", "pdu2.lindenlab.com"])
+        
