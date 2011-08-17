31fe6e3ad348891039b4e019c7b7ab5c51acd6b2
Joshua Tobin <joshuatobin@gmail.com>
2011-08-17 21:07:23 +0000

    Adding locking view to assist with dns updates

---
+++ src/jinx_api/api/lib/locking.py
@@ -0,0 +1,22 @@
+from jinx_api.api.models import Lock
+from django.core.exceptions import ObjectDoesNotExist
+from jinx_client.jinx_exceptions import JinxDataNotFoundError
+
+def acquire_lock(lock):
+    """
+    Arguments:
+        lock -- Name of lock
+
+    Exceptions Raised:
+        JinxDataNotFoundError -- Lock name not found.
+    """
+
+    try:
+        l = Lock.objects.get(name=lock)
+        l.value += 1
+        l.save()
+    except ObjectDoesNotExist:
+        raise JinxDataNotFoundError("Error: Could not find lock: %s. Please use a lock that already exists." %s lock)
+        
+    
+    
