5919c10047c6c88f67d64a12f5526a607f8c67d1
Joshua Tobin <joshuatobin@gmail.com>
2011-08-17 18:05:00 +0000

    Allowing comments to be blank for DNS models

--- src/jinx_api/api/models.py
+++ src/jinx_api/api/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class DNSService(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    comment = models.CharField(max_length=255)
+    comment = models.CharField(max_length=255, blank=True)
 
     def __unicode__(self):
         return self.name
 
 class DNSRecord(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    comment = models.CharField(max_length=255)
+    comment = models.CharField(max_length=255, blank=True)
     group = models.ForeignKey(DNSService, null=True)
 
     def __unicode__(self):
